e715aebd65a335263a94d3e7416a341cebffa97d
Collin Smith <collink33@gmail.com>
2016-05-10 13:28:28 -0700

    Set up babel & Relay

--- js/components/App.js
+++ js/components/App.js
@@ -3,11 +3,26 @@ import Relay from 'react-relay';
 
 class App extends React.Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div>{this.props.product.name}</div>
+    );
   }
 }
 
 export default Relay.createContainer(App, {
   fragments: {
+    product: () => Relay.QL`
+      fragment on Product {
+        name
+        calories
+        reviews {
+          reviewId
+          title
+          detail
+          nickname
+          rating
+        }
+      }
+    `,
   },
 });
--- js/routes/AppHomeRoute.js
+++ js/routes/AppHomeRoute.js
@@ -3,7 +3,7 @@ import Relay from 'react-relay';
 export default class extends Relay.Route {
   static path = '/';
   static queries = {
-
+    product: () => Relay.QL`query { product(id: "1325") }`,
   };
   static routeName = 'AppHomeRoute';
 }
