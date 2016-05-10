b153f0e2c3500326105d47c395deb2f4ec8d1718
Collin Smith <collink33@gmail.com>
2016-05-10 13:50:02 -0700

    Display some product data in main app

--- js/components/App.js
+++ js/components/App.js
@@ -4,7 +4,21 @@ import Relay from 'react-relay';
 class App extends React.Component {
   render() {
     return (
-      <div>{this.props.product.name}</div>
+      <div>
+        <h1>{this.props.product.name}</h1>
+        <h2>Reviews:</h2>
+        <ul>
+          {this.props.product.reviews.map((review, idx) => {
+            return (
+              <li key={idx}>
+                <h3>{review.title}</h3>
+                <p><em>by {review.nickname}</em></p>
+                <p>{review.detail}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     );
   }
 }
